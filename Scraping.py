32f30c93918cb7ec92bfc0d2e2dccdc06cb8e311
mayowill303 <96395120+mayowill303@users.noreply.github.com>
2022-03-11 19:48:28 -0600

    Module 10 Challenge

--- Scraping.py
+++ Scraping.py
@@ -1,8 +1,10 @@
 # Import Splinter, BeautifulSoup, and Pandas
+from itsdangerous import NoneAlgorithm
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
+import datetime as dt
 
 # Set up Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -31,7 +33,6 @@ def scrape_all():
 def mars_news(browser):
 
     # Scrape Mars News
-
     # Visit the Mars news site
     url = 'https://redplanetscience.com/'
     browser.visit(url)
@@ -42,22 +43,26 @@ def mars_news(browser):
     # Convert the browser html to a soup object and then quit the browser
     html = browser.html
     news_soup = soup(html, 'html.parser')
-
+    
+    # Add try/except for error handling
+    try:
         slide_elem = news_soup.select_one('div.list_text')
-
         slide_elem.find('div', class_='content_title')
 
     # Use the parent element to find the first a tag and save it as `news_title`
         news_title = slide_elem.find('div', class_='content_title').get_text()
-        news_title
 
     # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-        news_p
+
+    except AttributeError:
+        return None, None
+
+    return news_title, news_p
 
 # ## JPL Space Images Featured Image
 
-def featured_image(browser)
+def featured_image(browser):
     # Visit URL
     url = 'https://spaceimages-mars.com'
     browser.visit(url)
@@ -66,24 +71,28 @@ def featured_image(browser)
     full_image_elem = browser.find_by_tag('button')[1]
     full_image_elem.click()
 
+    # Find the more info button and click that
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.links.find_by_partial_text('more info')
+    more_info_elem.click()
+
     # Parse the resulting html with soup
     html = browser.html
     img_soup = soup(html, 'html.parser')
-    img_soup
 
     # Add try/except for error handling
     try:
 
         # find the relative image url
         img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-        img_url_rel
     
     except AttributeError:
         return None
     
     # Use the base url to create an absolute url
     img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-    img_url
+    
+    return img_url
 
 # ## Mars Facts
 
@@ -92,7 +101,6 @@ def mars_facts():
     try:
         # Use 'read_html' to scrape the facts table into a dataframe
         df = pd.read_html('https://galaxyfacts-mars.com')[0]
-        df.head()
 
     except BaseException:
         return None
@@ -100,7 +108,6 @@ def mars_facts():
     # Assign columns and set index of dataframe
     df.columns=['Description', 'Mars', 'Earth']
     df.set_index('Description', inplace=True)
-    df
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
@@ -114,7 +121,6 @@ def hemispheres(browser):
 
     # 1. Use browser to visit the URL 
     url = 'https://marshemispheres.com/'
-
     browser.visit(url)
 
     # Parse the resulting html with soup
@@ -125,6 +131,7 @@ def hemispheres(browser):
     # 2. Create a list to hold the images and titles.
     hemisphere_image_urls = []
 
+# items = browser.find_by_css('a.product-item h3')
     # 3. Write code to retrieve the image urls and titles for each hemisphere.
     for i in range(4):
         browser.find_by_css('a.product-item h3')[i].click()
@@ -137,12 +144,10 @@ def hemispheres(browser):
         browser.back()
 
     # 4. Print the list that holds the dictionary of each image url and title.
-    hemisphere_image_urls
+    return hemisphere_image_urls
 
     # 5. Quit the browser
     browser.quit()
-
-return hemisphere_image_urls
 
 def scrape_hemisphere(html_text):
     # parse html text
--- app.py
+++ app.py
@@ -1,164 +1,26 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 39,
-   "id": "0bd098de",
-   "metadata": {},
-   "outputs": [
-    {
-     "ename": "ModuleNotFoundError",
-     "evalue": "No module named 'scraping'",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[1;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[1;31mModuleNotFoundError\u001b[0m                       Traceback (most recent call last)",
-      "\u001b[1;32m~\\AppData\\Local\\Temp/ipykernel_72080/379545566.py\u001b[0m in \u001b[0;36m<module>\u001b[1;34m\u001b[0m\n\u001b[0;32m      1\u001b[0m \u001b[1;32mfrom\u001b[0m \u001b[0mflask\u001b[0m \u001b[1;32mimport\u001b[0m \u001b[0mFlask\u001b[0m\u001b[1;33m,\u001b[0m \u001b[0mrender_template\u001b[0m\u001b[1;33m,\u001b[0m \u001b[0mredirect\u001b[0m\u001b[1;33m,\u001b[0m \u001b[0murl_for\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0;32m      2\u001b[0m \u001b[1;32mfrom\u001b[0m \u001b[0mflask_pymongo\u001b[0m \u001b[1;32mimport\u001b[0m \u001b[0mPyMongo\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[1;32m----> 3\u001b[1;33m \u001b[1;32mimport\u001b[0m \u001b[0mscraping\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0m",
-      "\u001b[1;31mModuleNotFoundError\u001b[0m: No module named 'scraping'"
-     ]
-    }
-   ],
-   "source": [
-    "from flask import Flask, render_template, redirect, url_for\n",
-    "from flask_pymongo import PyMongo\n",
-    "import scraping"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 40,
-   "id": "9c67d783",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "app = Flask(__name__)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 41,
-   "id": "e26e1dc5",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Use flask_pymongo to set up mongo connection\n",
-    "app.config[\"MONGO_URI\"] = \"mongodb://localhost:27017/mars_app\"\n",
-    "mongo = PyMongo(app)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 42,
-   "id": "741d5a0e",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#creating route to find HTML page \n",
-    "@app.route(\"/\")\n",
-    "def index():\n",
-    "   mars = mongo.db.mars.find_one()\n",
-    "   return render_template(\"index.html\", mars=mars)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "25896b98",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      " * Serving Flask app \"__main__\" (lazy loading)\n",
-      " * Environment: production\n",
-      "\u001b[31m   WARNING: This is a development server. Do not use it in a production deployment.\u001b[0m\n",
-      "\u001b[2m   Use a production WSGI server instead.\u001b[0m\n",
-      " * Debug mode: off\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      " * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)\n",
-      "[2022-03-10 23:32:31,955] ERROR in app: Exception on / [GET]\n",
-      "Traceback (most recent call last):\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\flask\\app.py\", line 2447, in wsgi_app\n",
-      "    response = self.full_dispatch_request()\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\flask\\app.py\", line 1952, in full_dispatch_request\n",
-      "    rv = self.handle_user_exception(e)\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\flask\\app.py\", line 1821, in handle_user_exception\n",
-      "    reraise(exc_type, exc_value, tb)\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\flask\\_compat.py\", line 39, in reraise\n",
-      "    raise value\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\flask\\app.py\", line 1950, in full_dispatch_request\n",
-      "    rv = self.dispatch_request()\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\flask\\app.py\", line 1936, in dispatch_request\n",
-      "    return self.view_functions[rule.endpoint](**req.view_args)\n",
-      "  File \"C:\\Users\\M233370\\AppData\\Local\\Temp/ipykernel_72080/3712898523.py\", line 5, in index\n",
-      "    return render_template(\"index.html\", mars=mars)\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\flask\\templating.py\", line 138, in render_template\n",
-      "    ctx.app.jinja_env.get_or_select_template(template_name_or_list),\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\jinja2\\environment.py\", line 930, in get_or_select_template\n",
-      "    return self.get_template(template_name_or_list, parent, globals)\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\jinja2\\environment.py\", line 883, in get_template\n",
-      "    return self._load_template(name, self.make_globals(globals))\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\jinja2\\environment.py\", line 857, in _load_template\n",
-      "    template = self.loader.load(self, name, globals)\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\jinja2\\loaders.py\", line 115, in load\n",
-      "    source, filename, uptodate = self.get_source(environment, name)\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\flask\\templating.py\", line 60, in get_source\n",
-      "    return self._get_source_fast(environment, template)\n",
-      "  File \"C:\\Users\\M233370\\Anaconda3\\lib\\site-packages\\flask\\templating.py\", line 89, in _get_source_fast\n",
-      "    raise TemplateNotFound(template)\n",
-      "jinja2.exceptions.TemplateNotFound: index.html\n",
-      "127.0.0.1 - - [10/Mar/2022 23:32:31] \"GET / HTTP/1.1\" 500 -\n"
-     ]
-    }
-   ],
-   "source": [
-    "#scraping route\n",
-    "\n",
-    "@app.route(\"/scrape\")\n",
-    "def scrape():\n",
-    "   mars = mongo.db.mars\n",
-    "   mars_data = scraping.scrape_all()\n",
-    "   mars.update({}, mars_data, upsert=True)\n",
-    "   return \"Scraping Successful!\"\n",
-    "\n",
-    "if __name__ == \"__main__\":\n",
-    "   app.run()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "2bad9e2b",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "PythonData",
-   "language": "python",
-   "name": "pythondata"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+#creating route to find HTML page 
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+#scraping route
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+if __name__ == "__main__":
+   app.run()
